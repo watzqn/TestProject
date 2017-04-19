ec2e91fa57c9dca8156ea1ead0174a6b20e88a2c
watzqn <klussmann.luca@gmail.com>
2017-04-18 23:19:25 -0500

    New files

---
+++ TestProject/Assets/Scripts/CameraActive.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraActive : MonoBehaviour {
+
+    //variables
+    public GameObject Camera1;
+    public GameObject Camera2;
+    public GameObject Player1;
+    public GameObject Player2;
+    PlayerMovement Script1;
+    PlayerMovement Script2;
+	
+    void Start()
+    {
+        //assigning script variables to actual scripts
+        Script1 = Player1.GetComponent<PlayerMovement>();
+        Script2 = Player2.GetComponent<PlayerMovement>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        //Change Camera and Script on Button Press
+		if(Input.GetKeyDown(KeyCode.F))
+        {
+            if (Camera1.activeInHierarchy == true)
+            {
+                Camera1.SetActive(false);
+                Camera2.SetActive(true);
+
+                Script1.enabled = false;
+                Script2.enabled = true;
+            }
+            else
+            {
+                Camera2.SetActive(false);
+                Camera1.SetActive(true);
+
+                Script2.enabled = false;
+                Script1.enabled = true;
+            }
+        }
+	}
+}
---
+++ TestProject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour {
+
+    //variables
+    public float speed = 4f;
+    public float sensitivity = 2f;
+    public GameObject Eyes;
+    float moveFB;
+    float moveLR;
+    float rotX;
+    float rotY;
+    float vertVelocity;
+    CharacterController player;
+
+	void Start () {
+        //gives access to the Player's Character Controller
+        player = GetComponent<CharacterController>();
+
+        //Locks and Hides Cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+	}
+	
+
+	void Update () {
+
+        //defining WASD movement
+        moveFB = Input.GetAxis("Vertical") * speed;
+        moveLR = Input.GetAxis("Horizontal") * speed;
+
+        //Sprint Script
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 8f;
+        }
+        else
+        {
+            speed = 4f;
+        }
+
+        //assigning moveFB and moveLR to the camera vectors, and vertVelocity for gravity
+        Vector3 movement = new Vector3(moveLR, vertVelocity, moveFB);
+
+        //WASD movement based on the Players rotation
+        movement = transform.rotation * movement;
+
+        //applies delta time to the movement of the character controller
+        player.Move(movement * Time.deltaTime);
+
+        //defining Mouse movement
+        rotX = Input.GetAxis("Mouse X") * sensitivity;
+        rotY -= Input.GetAxis("Mouse Y") * sensitivity;
+
+        //Limiting rotY angles
+        rotY = Mathf.Clamp(rotY, -70f, 70f);
+
+        //rotX takes over Y rotations
+        transform.Rotate(0, rotX, 0);
+
+        //rotY takes over X rotations
+        Eyes.transform.localRotation = Quaternion.Euler(rotY, 0, 0);
+	}
+
+    void FixedUpdate()
+    {
+        //applies gravity to the player
+        if (player.isGrounded == false)
+        {
+            vertVelocity += Physics.gravity.y * Time.deltaTime;
+        }
+        else
+        {
+            vertVelocity = 0f;
+        }
+    }
+}
